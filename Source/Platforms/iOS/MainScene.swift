5d604ca0a76e4c3ab1af9076d102e04766e43e53
asu <arsenyk.ustaris.dev@gmail.com>
2016-01-11 00:13:27 -0800

    Moves collision code into scene class

--- Source/Platforms/iOS/MainScene.swift
+++ Source/Platforms/iOS/MainScene.swift
@@ -8,23 +8,95 @@
 
 import UIKit
 
-class MainScene: CCNode, CCPhysicsCollisionDelegate{
+class MainScene: CCNode, CCPhysicsCollisionDelegate, UIGestureRecognizerDelegate{
     
     weak var gamePhysicsNode: CCPhysicsNode!
+    weak var sceneView: UIView!
+
+    var panDetector: UIPanGestureRecognizer!
+    var panStart:CGPoint?
+    var panShape:SubShapeNode?
+
     
     func didLoadFromCCB(){
 
+        
+        panDetector = UIPanGestureRecognizer(target: self, action:Selector("panning:"))
+        panDetector.delegate = self;
+
+        sceneView = CCDirector.sharedDirector().view
+        sceneView.addGestureRecognizer(panDetector)
+        
     	gamePhysicsNode.collisionDelegate = self
- 
+        print ("\(gamePhysicsNode)")
+        print ("\(gamePhysicsNode.children)")
+
+        
+    }
+
+    
+    func touchedNode(pan:UIPanGestureRecognizer) -> CCNode? {
+        return touchedNodeAtPoint(pan.locationInView(pan.view))
+        
+    }
+    
+    func touchedNodeAtPoint(touchPoint:CGPoint) -> CCNode? {
+        let touchLocation = CCDirector.sharedDirector().convertToGL(touchPoint)
+        let responder = CCDirector.sharedDirector().responderManager
+        let node = responder.nodeAtPoint(touchLocation)
+        
+        return node
+    }
+    
+    func gestureRecognizerShouldBegin(pan: UIGestureRecognizer) -> Bool {
+		let node = touchedNode(pan as! UIPanGestureRecognizer)
+        
+        if (node == nil){
+            return false;
+        }
+        
+        if (node!.isKindOfClass(SubShapeNode)){
+            return true
+        }
+        
+        return false;
     }
     
     
-    func ccPhysicsCollisionPostSolve(pair: CCPhysicsCollisionPair!, shapeA: SubShapeNode!, shapeB: SubShapeNode!) -> Bool {
+    
+    func panning(pan:UIPanGestureRecognizer){
+        let deltaVector = pan.translationInView(sceneView)
         
-        print ("COLLIDE")
-        shapeA.physicsBody.applyImpulse(pair.totalImpulse);
+        if (panStart == nil) {
+            panStart = pan.locationInView(sceneView)
+            panShape = (touchedNode(pan) as! SubShapeNode)
+        }
+    
+        let translatedLocation = panStart! + deltaVector
+        
+        let moveToPoint = CCDirector.sharedDirector().convertToGL(translatedLocation)
+        
+        panShape!.position = moveToPoint
+        
+        if (pan.state == .Ended){
+            panStart = nil;
+        }
+    }
+    
+    
+
+    
+    func ccPhysicsCollisionPostSolve(pair: CCPhysicsCollisionPair!, shape shapeA: SubShapeNode!, shape shapeB: SubShapeNode!) -> Bool {
+        print ("COLLIDE \(shapeA.identifier) vs \(shapeB.identifier)")
+
+        pair.restitution = 1.0
         
         return true;
     }
+    
+    
+
+    
+    
     
 }
--- Source/Platforms/iOS/SubShapeNode.swift
+++ Source/Platforms/iOS/SubShapeNode.swift
@@ -8,63 +8,21 @@
 
 import UIKit
 
-class SubShapeNode: CCSprite, UIGestureRecognizerDelegate {
-    var panDetector: UIPanGestureRecognizer!
+class SubShapeNode: CCSprite {
+    
+    var identifier: String?
     
     weak var sceneView: UIView!
-    
-    var panStart:CGPoint?
     
     func didLoadFromCCB(){
         userInteractionEnabled = true
         
-        panDetector = UIPanGestureRecognizer(target: self, action:Selector("panning:"))
-        panDetector.delegate = self;
-        
         sceneView = CCDirector.sharedDirector().view
-        sceneView.addGestureRecognizer(panDetector)
         
-        self.physicsBody.elasticity = 1000
+        self.zOrder = 0
         
+        self.physicsBody.friction = 1.0
+        self.physicsBody.elasticity = 0.0
     }
     
-
-    func gestureRecognizerShouldBegin(pan: UIGestureRecognizer) -> Bool {
-        let touchLocation = CCDirector.sharedDirector().convertToGL(pan.locationInView(pan.view))
-        let responder = CCDirector.sharedDirector().responderManager
-        let node = responder.nodeAtPoint(touchLocation)
-        
-        if (node == nil){
-            return false;
-        }
-        
-        if (node == self){
-            return true
-        }
-        
-        return false;
-    }
-    
-
-    
-    func panning(pan:UIPanGestureRecognizer){
-        let deltaVector = pan.translationInView(sceneView)
-
-        if (panStart == nil) {
-            panStart = pan.locationInView(sceneView)
-        }
-        
-        let translatedLocation = panStart! + deltaVector
-
-        let moveToPoint = CCDirector.sharedDirector().convertToGL(translatedLocation)
-
-        self.position = moveToPoint
-        
-        if (pan.state == .Ended){
-            print ("panning: Ended")
-            panStart = nil;
-        }
-    }
-    
-   
 }
